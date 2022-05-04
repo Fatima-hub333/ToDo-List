c5455ffc6f7c4f59a718165dc32f6b1a01adb8e8
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-04 12:03:46 +0500

    Create UI for list-structure ToDo-List

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const myTodo = [
+  {
+    task: 'Microverse recap',
+    progress: false,
+    index: 0,
+  },
+  {
+    task: 'Buy grocery',
+    progress: false,
+    index: 1,
+  },
+  {
+    task: 'Cleaning house',
+    progress: false,
+    index: 2,
+  },
+  {
+    task: 'Loundery',
+    progress: false,
+    index: 3,
+  },
+];
+const toDoListFunc = () => {
+  const sect = document.getElementById('myList');
+  for (let i = 0; i <= myTodo.length; i += 1) {
+    const toDoList = document.createElement('div');
+    toDoList.className = 'addedTasks';
+    const script = `<input type="checkbox" id="list" name="list" value="text">
+<label for="vehicle1">${myTodo[i].task}</label>
+<div class='icons'>
+<i class="fa-solid fa-ellipsis-vertical"></i>
+</div>
+<hr>`;
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+    toDoList.innerHTML = script;
+    sect.appendChild(toDoList);
+  }
+};
+toDoListFunc();
