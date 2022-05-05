db423f24c0aa71615d068a00671c7058850f3375
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-05 19:19:10 +0500

    Install webpack

--- src/index.js
+++ src/index.js
@@ -1,41 +1,10 @@
-import './style.css';
+function component() {
+  const element = document.createElement('div');
 
-const myTodo = [
-  {
-    task: 'Microverse recap',
-    progress: false,
-    index: 0,
-  },
-  {
-    task: 'Buy grocery',
-    progress: false,
-    index: 1,
-  },
-  {
-    task: 'Cleaning house',
-    progress: false,
-    index: 2,
-  },
-  {
-    task: 'Loundery',
-    progress: false,
-    index: 3,
-  },
-];
-const toDoListFunc = () => {
-  const sect = document.getElementById('myList');
-  for (let i = 0; i <= myTodo.length; i += 1) {
-    const toDoList = document.createElement('div');
-    toDoList.className = 'addedTasks';
-    const script = `<input type="checkbox" id="list" name="list" value="text">
-<label for="vehicle1">${myTodo[i].task}</label>
-<div class='icons'>
-<i class="fa-solid fa-ellipsis-vertical"></i>
-</div>
-<hr>`;
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    toDoList.innerHTML = script;
-    sect.appendChild(toDoList);
-  }
-};
-toDoListFunc();
+  return element;
+}
+
+document.body.appendChild(component());
