5e9f0bb744d479435ecc88297018429c5b1d9cdd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-10 22:23:29 +0500

    fix issues in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
+  mode: 'development',
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Webpack Boilerplate',
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
-
   optimization: {
     runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
