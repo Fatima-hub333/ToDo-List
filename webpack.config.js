fdd05e23cf61f0d74af02f87d483dbe0f3b8e795
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-10 22:32:05 +0500

    Fix issues

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
